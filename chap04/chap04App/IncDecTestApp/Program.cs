918e3b7d8026df85e079455c72367291c48d28e3
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-02-22 17:27:22 +0900

    4장 커밋 2월 22일

---
+++ chap04/chap04App/IncDecTestApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IncDecTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("증감연산자 테스트");
+
+            int result = 26;
+            Console.WriteLine($"현재 숫자는 {result}");
+            result += 3;
+            Console.WriteLine($"현재 숫자는 {result}");
+            result -= 10;
+            Console.WriteLine($"현재 숫자는 {result}");
+
+            Console.WriteLine($"현재 숫자는 {result++}");
+
+            Console.WriteLine($"현재 숫자는 {result}");
+
+            Console.WriteLine($"현재 숫자는 {++result}");
+
+            Console.WriteLine($"전치감소 숫자는 {--result}");
+
+            Console.WriteLine($"후치감소 숫자는 {result--}");
+
+            Console.WriteLine($"현재 숫자는 {result}");
+
+        }
+    }
+}
---
+++ chap04/chap04App/chap04App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chap04App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = 37656234;
+            if (values % 2 == 0)
+            {
+                Console.WriteLine("짝수입니다");
+
+            }
+            else
+            {
+                Console.WriteLine("홀수입니다");
+            }
+
+            if (values % 3 == 0)
+            {
+                Console.WriteLine("3의배수 입니다");
+
+            }
+            else
+            {
+                Console.WriteLine("3의배수가 아닙니다");
+            }
+            if (values % 7 == 0)
+            {
+                Console.WriteLine("7의배수 입니다");
+
+            }
+            else
+            {
+                Console.WriteLine("7의배수가 아닙니다");
+            }
+
+        }
+    }
+}
