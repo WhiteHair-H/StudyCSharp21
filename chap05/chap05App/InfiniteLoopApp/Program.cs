eef3e05b8245112fa79354d4c9fb15b5bd3469ca
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-02-23 14:51:31 +0900

    5장 커밋

---
+++ chap05/chap05App/InfiniteLoopApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InfiniteLoopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            long idx = 0;
+            for ( ; ; )
+            {
+                Console.WriteLine($"idx = {idx++}");
+            }
+        }
+    }
+}
---
+++ chap05/chap05App/StarTestApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StarTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("별모양찍기");
+
+            #region 첫번째별모양
+
+            
+
+             for (int i = 0; i < 5; i++) // 행
+             {
+                 for (int j = 0; j < 5-i; j++) // 열
+                 {
+                     Console.Write(" ");
+                 }
+
+                for (int j = 0; j < i + 1; j++)
+                {
+                    Console.Write("*");
+                }
+                 Console.WriteLine("");
+
+
+
+             }
+            #endregion
+        }
+    }
+}
