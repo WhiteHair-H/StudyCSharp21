b69f7a4b82bcc9cb9cd9ce666126f041200f6911
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-02-26 17:21:46 +0900

    10장 커밋

---
+++ chap10/Chap10App/2DArrayTestApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _2DArrayTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arr = new int[2, 3];
+            arr[0, 0] = 1;
+            arr[0, 1] = 2;
+            arr[0, 2] = 3;
+            arr[1, 0] = 4;
+            arr[1, 1] = 5;
+            arr[1, 2] = 6;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int z = 0; z < arr.GetLength(1); z++)
+                {
+                    Console.Write($"[{i}, {z}] : {arr[i, z]}\t");
+                }
+                Console.WriteLine("");
+            }
+
+
+            
+
+        }
+    }
+}
--- chap10/Chap10App/Chap10App/Program.cs
+++ chap10/Chap10App/Chap10App/Program.cs
@@ -2,12 +2,94 @@
 
 namespace Chap10App
 {
+    class Test
+    { }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            int[] scores = new int[5];
-            scores = [80, 74, 81, 90, 34];
+            int[] scores = { 80, 74, 90, 65, 10 }; /*new int[5];
+            scores[0] = 5;
+            scores[1] = 10;
+            scores[2] = 80;
+            scores[3] = 74;
+            scores[4] = 90;*/
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"{i}의 점수는 {scores[i]}");
+            }
+
+            Array.Resize ( ref scores, 6 );
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"{i}의 점수는 {scores[i]}");
+            }
+
+            int idx = Array.IndexOf(scores, 81);
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"{i}의 점수는 {scores[i]}");
+            }
+
+
+
+            /*Console.WriteLine("힘든 방법");
+
+            Console.WriteLine($"점수는 {scores[0]}");
+            Console.WriteLine($"점수는 {scores[1]}");
+            Console.WriteLine($"점수는 {scores[2]}");
+            Console.WriteLine($"점수는 {scores[3]}");
+            Console.WriteLine($"점수는 {scores[4]}\n");
+
+            Console.WriteLine($"Type of array : {scores.GetType()}");
+            Console.WriteLine($"Base Type of array : {scores.GetType().BaseType}");
+
+
+
+            Console.WriteLine("foreach 방법");
+            foreach (var score in scores)
+            {
+                Console.WriteLine($"점수는 {score}");
+            }
+
+
+            Console.WriteLine("\nfor 방법");
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"점수는 {scores[i]}");
+            }*/
+
+            /*Console.WriteLine("\n오름차순 정렬");
+            Array.Sort(scores);
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"점수는 {scores[i]}");
+            }
+
+            Console.WriteLine("\n내림차순 정렬");
+            Array.Reverse(scores);
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"점수는 {scores[i]}");
+            }
+
+            int idx = Array.IndexOf(scores,74);
+            Console.WriteLine($"{idx}");
+
+            int idx1 = Array.IndexOf(scores, 100);
+            Console.WriteLine($"{idx1}");
+
+            Array.Clear(scores, 3, 2);
+            for (var i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine($"{scores[i]}");
+            }*/
+
+
+
         }
+
     }
 }
