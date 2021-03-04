95f87c55ff60a09d5c351a6db6c13de08acb1fe0
WhiteHair-H <https://github.com/WhiteHair-H/BasicSetting.git>
2021-03-04 09:22:06 +0900

    18장 커밋

--- chap18/Chap18App/TextFileApp/Program.cs
+++ chap18/Chap18App/TextFileApp/Program.cs
@@ -11,30 +11,39 @@ namespace TextFileApp
     {
         static void Main(string[] args)
         {
-            string filepath = @"D:\GitRepository\StudyCSharp21\SampleDir\subFolder";
+            string filePath = @"D:\GitRepository\StudyCSharp21\SampleDir\SubFolder\a.dat"; // 텍스트 파일 생성위치
 
             StreamWriter sw = null;
 
-
             try
             {
-                sw = new StreamWriter(new FileStream(filepath, FileMode.Create));
-                sw.WriteLine(int.MaxValue);
-                sw.WriteLine("Hello , World");
+                sw = new StreamWriter(new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write));
+                sw.WriteLine($"int.MaxValue = {int.MaxValue}");
+                sw.WriteLine("Hello, World!");
+                sw.WriteLine($"uint.MaxValue = {uint.MaxValue}");
+                sw.WriteLine("안녕하세요!!");
+                sw.WriteLine($"double.MaxValue = {double.MaxValue}");
 
+                Console.WriteLine("파일 생성");
             }
             catch (Exception ex)
             {
-
                 Console.WriteLine($"파일쓰기 예외발생: {ex.Message}");
             }
-
             finally
             {
-
+                sw.Close();
             }
-            sw.Close();
 
+            StreamReader sr = null;
+            sr = new StreamReader(new FileStream(filePath, FileMode.Open, FileAccess.Read));
+            Console.WriteLine($"File size : {sr.BaseStream.Length} bytes");
+
+            while (sr.EndOfStream == false)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+            sr.Close();
         }
     }
 }
